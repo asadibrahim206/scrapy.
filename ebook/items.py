95842f9f161c307f1d348d7a8491421ba3f26d15
asadibrahim206 <asad.denin@gmail.com>
2025-12-02 16:21:57 +0500

    new file

--- ebook/items.py
+++ ebook/items.py
@@ -1,9 +1,17 @@
 
 from scrapy import Item,Field
+from itemloaders.processors import MapCompose,TakeFirst
 
+
+def get_number(txt):
+    return float(txt.replace("$",""))
 
 class EbookItem(Item):
     # define the fields for your item here like:
     title = Field()
-    price = Field()
+    price = Field(
+
+        input_processor = MapCompose(get_number)
+        output_processor = TakeFirst()
+    )
    
